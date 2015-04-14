74bf04db5c205cb855a070f80d7ec8ad19b750a5
Dustin Myers <dustint314@gmail.com>
2015-04-14 16:39:27 -0600

    first commit, project finished, now adding material design

---
+++ mainController.js
@@ -0,0 +1,32 @@
+var app = angular.module('quoteBook');
+
+app.controller('mainControl', function($scope, mainService) {
+
+	$scope.addDataQuote;
+	$scope.addDataAuthor;
+	$scope.removeDataInput;
+	$scope.filterData;
+	$scope.removeQuote;
+
+	$scope.quotes = mainService.getData();
+
+	$scope.addData =  function() {
+		var newObj = {
+			text: $scope.addDataQuote,
+			author: $scope.addDataAuthor
+		}
+		mainService.addData(newObj);
+	}
+
+	$scope.removeData =  function() {
+		var inputStr = $scope.removeDataInput;
+		mainService.removeData(inputStr);
+	}
+
+	$scope.removeQuote = function(index) {
+        mainService.removeQuote(index);
+		console.log("Remove button clicked")
+	}
+
+
+});
